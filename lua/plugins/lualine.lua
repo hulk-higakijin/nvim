c4f1da8af4edc083ac870743bf091e7517378589
hulk-higakijin <hm385.chejptks@gmail.com>
2024-12-28 14:15:11 +0900

    feat: show full path with lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  optional = true,
+  opts = function(_, opts)
+    table.remove(opts.sections.lualine_c, 1)
+    table.remove(opts.sections.lualine_c, #opts.sections.lualine_c)
+    table.insert(opts.sections.lualine_c, {
+      "filename",
+      path = 3,
+    })
+  end,
+}
