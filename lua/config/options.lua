506ff40be5a2bfdd8f79d719a0515c491baf780c
hulk-higakijin <hm385.chejptks@gmail.com>
2025-02-01 10:50:25 +0900

    feat: scale for neovide

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,14 @@
 -- Add any additional options here
 --
 vim.opt.relativenumber = false
+
+vim.g.neovide_scale_factor = 0.8
+local change_scale_factor = function(delta)
+  vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+end
+vim.keymap.set("n", "<C-=>", function()
+  change_scale_factor(1.25)
+end)
+vim.keymap.set("n", "<C-->", function()
+  change_scale_factor(1/1.25)
+end)
