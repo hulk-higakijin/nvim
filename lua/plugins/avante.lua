40246a440c285d6112981abdf1b081980b70573e
hulk-higakijin <hm385.chejptks@gmail.com>
2025-05-24 11:05:59 +0900

    feat: gpt-4.1

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -11,13 +11,10 @@ return {
       hints = { enabled = false },
 
       ---@alias AvanteProvider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
-      provider = "claude", -- Recommend using Claude
-      auto_suggestions_provider = "claude", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
-      claude = {
-        endpoint = "https://api.anthropic.com",
-        model = "claude-3-5-sonnet-20241022",
-        temperature = 0,
-        max_tokens = 4096,
+      provider = "copilot", -- Recommend using Claude
+      auto_suggestions_provider = "copilot", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
+      copilot = {
+        model = "gpt-4.1",
       },
 
       -- File selector configuration
