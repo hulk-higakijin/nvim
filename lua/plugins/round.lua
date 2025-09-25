9de98a5c33231252503c51c7298cccac7d5c5a4a
hulk-higakijin <hm385.chejptks@gmail.com>
2025-09-25 10:15:48 +0900

    chore: renamed

--- lua/plugins/round.lua
+++ lua/plugins/round.lua
@@ -1,7 +1,7 @@
  -- https://github.com/LazyVim/LazyVim/issues/2708
 return {
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = {
       ui = {
         border = "rounded",
