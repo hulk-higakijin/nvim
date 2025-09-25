1bf87f514c95bfc625e05a03fd573437136226c9
hulk-higakijin <hm385.chejptks@gmail.com>
2025-09-25 10:55:15 +0900

    feat: higakijin

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,6 +3,37 @@ return {
   opts = {
     bigfile = { enabled = true },
     quickfile = { enabled = true },
+    dashboard = {
+      enabled = true,
+      sections = {
+        { section = "header" },
+        { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        { section = "startup" },
+      },
+      preset = {
+        header = [[
+    ██╗  ██╗██╗ ██████╗  █████╗ ██╗  ██╗██╗     ██╗██╗███╗   ██╗
+    ██║  ██║██║██╔════╝ ██╔══██╗██║ ██╔╝██║     ██║██║████╗  ██║
+    ███████║██║██║  ███╗███████║█████╔╝ ██║     ██║██║██╔██╗ ██║
+    ██╔══██║██║██║   ██║██╔══██║██╔═██╗ ██║██   ██║██║██║╚██╗██║
+    ██║  ██║██║╚██████╔╝██║  ██║██║  ██╗██║╚█████╔╝██║██║ ╚████║
+    ╚═╝  ╚═╝╚═╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝ ╚════╝ ╚═╝╚═╝  ╚═══╝
+        ]],
+        keys = {
+          -- { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.picker()" },
+          -- { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          -- { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.picker.grep()" },
+          -- { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.picker.recent()" },
+          -- { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.picker.files(vim.fn.stdpath('config'))" },
+          -- { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          -- { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+          -- { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+          -- { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+      },
+    },
     terminal = {
       win = {
         position = "float",
