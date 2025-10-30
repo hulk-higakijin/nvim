9254ffc5f61954d6934f3f89796393308135a3ec
hulk-higakijin <hm385.chejptks@gmail.com>
2025-10-30 20:19:06 +0900

    feat: git blame

--- lua/plugins/blamer.lua
+++ lua/plugins/blamer.lua
@@ -3,5 +3,7 @@ return {
   init = function()
     vim.g.blamer_enabled = true
     vim.g.blamer_date_format = "%Y/%m/%d %H:%M"
+    vim.g.blamer_show_in_visual_modes = 0
+    vim.g.blamer_show_in_insert_modes = 0
   end,
 }
