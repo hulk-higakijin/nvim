8b9cec97045c2fec07f2b7e2c720e9c872897fa0
hulk-higakijin <hm385.chejptks@gmail.com>
2025-10-30 15:44:56 +0900

    feat: copit & diffview

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Diffview toggle keymap
+vim.keymap.set("n", "<leader>dv", function()
+  local view = require("diffview.lib").get_current_view()
+  if view then
+    vim.cmd("DiffviewClose")
+  else
+    vim.cmd("DiffviewOpen")
+  end
+end, { desc = "Toggle Diffview" })
