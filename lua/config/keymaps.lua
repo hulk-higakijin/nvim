cb065ca5dfee974916bb2b76933b91fc3487173e
hulk-higakijin <hm385.chejptks@gmail.com>
2025-10-30 15:54:57 +0900

    feat: diffview keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,8 +6,19 @@
 vim.keymap.set("n", "<leader>dv", function()
   local view = require("diffview.lib").get_current_view()
   if view then
+    -- より確実にDiffviewを閉じる
+    view:close()
     vim.cmd("DiffviewClose")
   else
     vim.cmd("DiffviewOpen")
   end
 end, { desc = "Toggle Diffview" })
+
+-- Diffview with branch comparison
+vim.keymap.set("n", "<leader>db", function()
+  vim.ui.input({ prompt = "Compare with branch: " }, function(branch)
+    if branch and branch ~= "" then
+      vim.cmd("DiffviewOpen " .. branch)
+    end
+  end)
+end, { desc = "Diff with branch" })
