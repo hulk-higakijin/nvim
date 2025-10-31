043794de79ed47792a1118bd283d9f87c58add71
hulk-higakijin <hm385.chejptks@gmail.com>
2025-10-31 19:53:47 +0900

    chore: barbecue

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,10 +4,6 @@ return {
   opts = function(_, opts)
     table.remove(opts.sections.lualine_c, 1)
     table.remove(opts.sections.lualine_c, #opts.sections.lualine_c)
-    table.insert(opts.sections.lualine_c, {
-      "filename",
-      path = 3,
-    })
   end,
 }
 
