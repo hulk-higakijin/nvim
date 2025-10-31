897b516ca0231116bf99b6fbf58cdb990e22c11d
hulk-higakijin <hm385.chejptks@gmail.com>
2025-10-31 20:41:00 +0900

    fix: exclude overseer buffers from auto-save to enable popup UI

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -7,5 +7,21 @@ return {
         return ""
       end,
     },
+    condition = function(buf)
+      local fn = vim.fn
+      local utils = require("auto-save.utils.data")
+      
+      -- overseerのバッファを除外
+      local filetype = vim.api.nvim_buf_get_option(buf, "filetype")
+      if filetype == "overseer" or filetype == "OverseerList" or filetype == "OverseerForm" then
+        return false
+      end
+      
+      -- デフォルトの除外条件
+      if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
+        return true
+      end
+      return false
+    end,
   },
 }
