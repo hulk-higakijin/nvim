35c1de4e4958d0bd041b435eeb5a3fa24f1e8da8
higakijin <higakijin@higakijinnoMac-mini.local>
2026-03-14 19:08:55 +0900

    feat: Markdownの警告を無効化

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -7,6 +7,14 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   end,
 })
 
+-- markdownのみスペルチェックを無効化
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.spell = false
+  end,
+})
+
 -- Rubyファイルでの自動整形を無効化
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "ruby",
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,6 +1,11 @@
 return {
   {
     "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        marksman = false,
+      },
+    },
     init_options = {
       userLanguages = {
         eelixir = "html-eex",
