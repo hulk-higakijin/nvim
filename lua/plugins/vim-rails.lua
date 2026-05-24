0b56a8e1a3682d79d64acaf2060738ccf742ffea
hulk-higakijin <hm385.chejptks@gmail.com>
2026-05-24 12:39:41 +0900

    chore: vim-rails

---
+++ lua/plugins/vim-rails.lua
@@ -0,0 +1,11 @@
+return {
+  "tpope/vim-rails",
+  lazy = false,
+  keys = {
+    { "gd", "gf", remap = true, desc = "Go to file (gf)" },
+    { "<leader>ra", "<cmd>A<cr>", desc = "Rails alternate file" },
+    { "<leader>rm", "<cmd>Emodel<cr>", desc = "Rails model" },
+    { "<leader>rv", "<cmd>Eview<cr>", desc = "Rails view" },
+    { "<leader>rc", "<cmd>Econtroller<cr>", desc = "Rails controller" },
+  },
+}
